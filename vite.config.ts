109ffce3227b31d91e76fb445831e556fc938993
Phil Batey <pbatey@gmail.com>
2023-09-18 23:59:26 -0600

    fix chunk size
    - and fix css syntax error warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+function manualChunks(id:string) {
+	if (id.includes('node_modules')) {
+		return 'vendor'
+	}
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/crokinole-tourney/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks
+      }
+    }
+  },
 })
