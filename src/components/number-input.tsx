46820b54d4392c0b2110a9975c566507e24ed145
Phil Batey <pbatey@gmail.com>
2024-03-10 16:23:31 -0600

    fix: attempt to fix number input spinner icons on mobile

--- src/components/number-input.tsx
+++ src/components/number-input.tsx
@@ -45,21 +45,21 @@ export const NumberInput = React.forwardRef<HTMLInputElement, Props>(
       + ' disabled:cursor-not-allowed disabled:opacity-50'
       + ' border-input focus-visible:ring-ring'
       + ' [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
-      + ' focus:z-50 pl-1'
+      + ' focus:z-10 pl-1'
     const buttonClass =
       'flex h-9 shrink-0 border bg-transparent py-1 text-sm shadow-sm transition-colors'
       + ' placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50'
       + ' border-input focus-visible:ring-ring'
       + ' text-secondary-foreground shadow-sm hover:bg-secondary/80'
-      + ' focus:z-50 py-2.5 px-1'
+      + ' focus:z-10 py-2.5 px-1'
     const leftButtonClass = 'rounded-l-md border-r-0'
     const rightButtonClass = 'rounded-r-md border-l-0'
 
     const onIncrement = ()=>setValue(value+1)
     const onDecrement = ()=>setValue(value-1)
 
-    decrementLabel = decrementLabel || <MinusIcon size={'sm'}/>
-    incrementLabel = incrementLabel || <PlusIcon size={'sm'}/>
+    decrementLabel = decrementLabel || <MinusIcon size={'sm'} className='shrink-0'/>
+    incrementLabel = incrementLabel || <PlusIcon size={'sm'} className='shrink-0'/>
 
     return (
       <div className={cn(divClass, className)}>
@@ -71,7 +71,7 @@ export const NumberInput = React.forwardRef<HTMLInputElement, Props>(
           >{decrementLabel}</button>
         )}
         <input type="number" value={value} onChange={e=>onChange(e.target.value)}
-          className={cn(widthClass, inputClass, inputClassName)}
+          className={cn(widthClass, inputClass, inputClassName, readOnly ? 'rounded-md' : 'rounded-0')}
           {...props}
           ref={ref}
           disabled={disabled}
